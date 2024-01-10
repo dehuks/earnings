726b1e2203d43022b9cd3fec2f55271b4a58417d
Your Name <stevehuko@gmail.com>
2024-01-10 12:09:55 +0300

    added project

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,31 @@ public class Main {
         System.out.println("Enter buying price per share: ");
         double buyingPrice;
         buyingPrice = scan.nextDouble();
+        int day;
+        day = 1;
+        double closingPrice;
+
+        while (true){
+            System.out.println("Enter closing price day" + day + "(any negative value to leave the program)");
+            closingPrice = scan.nextDouble();
+            if(closingPrice < 0.0){
+                System.out.println("Exiting the program");
+                break;
+            }
+            double earnings = closingPrice - buyingPrice;
+            if(earnings > 0.0){
+                System.out.println("After day "+ day+ "you earned "+ earnings +" per share.");
+            }
+            else if(earnings < 0.0){
+                System.out.println("After day "+ day +"you have lost "+ (-earnings)+ " per share.");
+            }
+            else{
+                System.out.println("After day"+ day +",you have "+ "no earnings per share.");
+            }
+            day +=1;
+
+        }
+        scan.close();
 
     }
 }
